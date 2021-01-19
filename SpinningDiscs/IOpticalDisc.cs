a838bab642455bd5e3ecd0d64954e00f5ec73839
stacey <staceystroh.84@gmail.com>
2021-01-18 20:37:46 -0600

    comp

---
+++ SpinningDiscs/IOpticalDisc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpinningDiscs
+{
+    public interface IOpticalDisc
+    {
+        void SpinDisc();
+        void ReadData();
+    }
+
+}
